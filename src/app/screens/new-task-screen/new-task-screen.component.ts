b06d466f181372059325efa8cd7777ddec2ab390
towficaziz <tapolly01@gmail.com>
2022-10-13 14:18:54 +0200

    implementing add new task and mobile responsiv slv

--- src/app/screens/new-task-screen/new-task-screen.component.ts
+++ src/app/screens/new-task-screen/new-task-screen.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { TaskService } from './../../task.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewTaskScreenComponent implements OnInit {
 
-  constructor() { }
+  taskListId: string = "";
+  constructor(
+    private taskService: TaskService,
+    private activateRoute: ActivatedRoute,
+    private router: Router
+    ) {
+      this.activateRoute.params.subscribe(
+        (params: Params)=>{
+          this.taskListId = params['taskListId'];
+      });
+    }
 
   ngOnInit(): void {
+  }
+
+  addNewTask(title: string){
+    if(title){
+    this.taskService.createTaskInsideATaskList(this.taskListId, title)
+    .subscribe(()=>{
+      this.router.navigate(['../'], {relativeTo: this.activateRoute});
+    })
+  }else{
+    alert("Title cannot be empty!");
+    return;
+  }
   }
 
 }
